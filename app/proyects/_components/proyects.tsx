99d15bcede081826d11459124ce4d7f4c29a6844
NeroWork <84593907+NeroWork@users.noreply.github.com>
2024-09-19 13:11:53 -0300

    Some enhancement

--- app/proyects/_components/proyects.tsx
+++ app/proyects/_components/proyects.tsx
@@ -1,13 +1,13 @@
-export default function Proyects({name, description, img, github, tags}: {name:string, github:string, description:string, img:string, tags: string[]}){
+export default function Proyects({name, link, description, img, github, tags}: {name:string, link:string, github:string, description:string, img:string, tags: string[]}){
     return (
         <div className="flex rounded flex-row justify-center align-center mx-4 border-b-2 pb-4 mb-2 border-zinc-700">
-            <div className="flex flex-col w-[20%] h-full justify-center align-center mr-6 ml-4">
-                <img className="w-full h-fit" src={img} alt="" />
+            <div className="flex flex-col w-[25%] h-full justify-center align-center mr-6 ml-4">
+                <img className="rounded-md w-full h-fit" src={img} alt="" />
             </div>
-            <div className="w-full flex flex-col justify-around">
+            <div className="w-full flex flex-col justify-evenly">
                 <div className="flex flex-row justify-between align-center mr-6">
-                    <div className="text-2xl">{name}</div>
-                    <a className="flex" href={github}><img src="github.svg" className="w-6" alt="github icon" /></a>
+                    <a href={link} target="_blank"><div className="text-2xl">{name}</div></a>
+                    <a className="flex" target="_blank" href={github}><img src="github.svg" className="w-8" alt="github icon" /></a>
                 </div>
                 <div className="ml-1">{description}</div>
                 <div className="flex flex-row align-center mt-2">
--- app/proyects/page.tsx
+++ app/proyects/page.tsx
@@ -4,11 +4,10 @@ export default function Home() {
 
   return (
     <div className="grid py-6 bg-zinc-800 ml-[25%] min-h-[100vh] w-full">
-      <Proyects name="Nexus" github="#" img="https://images5.alphacoders.com/647/647235.jpg" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
-      <Proyects name="Nexus" github="#" img="https://images5.alphacoders.com/647/647235.jpg" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
-      <Proyects name="Nexus" github="#" img="https://images5.alphacoders.com/647/647235.jpg" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
-      <Proyects name="Nexus" github="#" img="https://images5.alphacoders.com/647/647235.jpg" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
-      <Proyects name="Nexus" github="#" img="https://images5.alphacoders.com/647/647235.jpg" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
+      <Proyects name="🛰️ Nexus" link="#" github="https://github.com/NeroWork/Nexus" img="https://images5.alphacoders.com/647/647235.jpg" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
+      <Proyects name="🗂️ To Do Web App" link="https://to-do-vj.vercel.app" github="https://github.com/NeroWork/to-do-vj" img="/todo.png" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
+      <Proyects name="🎴 Red Dev Redemption" link="https://blog-nero.vercel.app" github="https://github.com/NeroWork/blogNero" img="/blog.png" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
+      <Proyects name="🗿 Portafolio Web App" link="#" github="https://github.com/NeroWork/portafolio-2" img="/portafolio.png" description="E-commerce Web Page. It follows the design pattern of the most used platforms for buying and selling electronic devises, using top technologies." tags={["NextJS", "React", "MongoDB"]}></Proyects>
     </div>
   );
 }
